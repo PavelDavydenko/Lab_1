f070ea72dbb64f2713937ca9da654c71203949e2
PavelDavydenko <3dsworld@mail.ru>
2022-02-04 23:13:16 +0300

    up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,41 +7,43 @@ namespace ConsoleApp1
         {
             //Максимальная сумма двух чисел кратная 3
             Random rand = new Random();
-            int[] arr = new int[20];
+            Console.Write("Введите количество чисел: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+            int curentDigit = 0;
             int mod_1 = 0;
             int mod_2 = 0;
             int mod_3_1 = 0;
             int mod_3_2 = 0;
             Console.WriteLine("---------------------------------------------------------------------");
-            for (int i = 0;arr.Length>i;i++)
+            for (int i = 0;i<count;i++)
             {
-                arr[i] = rand.Next(1,100);
-                switch (arr[i] % 3)
+                curentDigit = rand.Next(1,100);
+                switch (curentDigit % 3)
                 {
                     case 0:
-                        Console.WriteLine(arr[i] + " - кратно 3");
+                        Console.WriteLine(curentDigit + " - кратно 3");
                         if (mod_3_1 > mod_3_2)
                         {
-                            if (arr[i] > mod_3_2){mod_3_2 = arr[i];}
+                            if (curentDigit > mod_3_2) {mod_3_2 = curentDigit;}
                         }
                         else 
                         {
-                            if (arr[i] > mod_3_1){mod_3_1 = arr[i];}
+                            if (curentDigit > mod_3_1) {mod_3_1 = curentDigit;}
                         }
                         break;
                     case 1:
-                        Console.WriteLine(arr[i] + " - остаток 1");
-                        if (arr[i] > mod_1) { mod_1 = arr[i]; }
+                        Console.WriteLine(curentDigit + " - остаток 1");
+                        if (curentDigit > mod_1) { mod_1 = curentDigit; }
                         break;
                     case 2:
-                        Console.WriteLine(arr[i] + " - остаток 2");
-                        if (arr[i] > mod_2) { mod_2 = arr[i]; }
+                        Console.WriteLine(curentDigit + " - остаток 2");
+                        if (curentDigit > mod_2) { mod_2 = curentDigit; }
                         break;
                 }
             }
             Console.WriteLine("---------------------------------------------------------------------");
-            Console.WriteLine("Сумма кратная 3: " + mod_1 + " + " + mod_2 + " = " + (mod_1 + mod_2));
-            Console.WriteLine("Сумма кратная 3: " + mod_3_1 + " + " + mod_3_2 + " = " + (mod_3_1 + mod_3_2));
+            Console.WriteLine("Максимальная сумма кратная 3 (из двух чисел некратных 3): " + mod_1 + " + " + mod_2 + " = " + (mod_1 + mod_2));
+            Console.WriteLine("Максимальная сумма кратная 3 (из двух чисел кратных 3): " + mod_3_1 + " + " + mod_3_2 + " = " + (mod_3_1 + mod_3_2));
             Console.WriteLine("---------------------------------------------------------------------");
             if (mod_1 + mod_2 > mod_3_1 + mod_3_2)
             { 
